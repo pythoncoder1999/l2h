096a7138c423ded0a81ccc210ecc18a2835da0d7
Madhav Subramaniyam <155390850+pythoncoder1999@users.noreply.github.com>
2025-10-14 11:21:05 -0400

    Update run_client.py

--- CIFAR10L2H/run_client.py
+++ CIFAR10L2H/run_client.py
@@ -26,7 +26,7 @@ parser.add_argument('--port', type=int, default=7000)  # Go proxy
 parser.add_argument('--start_at', type=int, default=None)  # epoch seconds barrier
 parser.add_argument('--conn_timeout', type=float, default=30.0)
 parser.add_argument('--retries', type=int, default=3)
-parser.add_argument('--thresholds', type=str, default='0.0')  # comma-separated
+parser.add_argument('--thresholds', type=str, default='-0.079675')  # comma-separated
 args = parser.parse_args()
 
 print("[clientV3] starting")
@@ -187,7 +187,7 @@ if __name__ == "__main__":
     norm = T.Normalize((0.4914, 0.4822, 0.4465),
                        (0.2023, 0.1994, 0.2010))
 
-    thresholds = [0.0]#[float(x) for x in args.thresholds.split(',')] if args.thresholds else [0.0]
+    thresholds = [-0.079675]#[float(x) for x in args.thresholds.split(',')] if args.thresholds else [0.0]
     iterations = 5
     client_cps = 0.0
 
